b5bef60537437809a40ffbab1e84de4e17d8201b
zuoshifan <zuoshifan@163.com>
2013-11-05 20:20:51 +0800

    Reorgnized the file structure, some codes may not run well due to the change of the file directorys.

--- visibility/constant.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-# Physical constants. All constants in SI units.
-k_B = 1.380658e-23          # Boltzmann constant
-c = 2.99792458e8            # Speed of light
-Omega_m = 0.27              # Matter content
-Hubble_h = 0.68             # Hubble constant h
-arcmin = np.pi / (60*180)   # Arcminute in unit rad
-deg2rad = np.pi/180.0       # Degree to rad
